8f93780fa253d1eca50eef4a3abb9fb753a74cbb
Piotr Markowski <piotrjakubmarkowski@gmail.com>
2017-04-20 15:26:06 +0200

    stworzenie klasy ComplexCommand

---
+++ src/test/java/edu/iis/powp/command/ComplexCommand.java
@@ -0,0 +1,22 @@
+package edu.iis.powp.command;
+
+import edu.iis.client.plottermagic.IPlotter;
+
+import java.util.List;
+
+/**
+ * Created by Piotr on 20.04.2017.
+ */
+public class ComplexCommand implements PlotterCommand{
+
+    private List<PlotterCommand> commands;
+
+    public ComplexCommand(List<PlotterCommand> commands) {
+        this.commands = commands;
+    }
+
+
+    @Override public void execute(IPlotter iPlotter) {
+
+    }
+}

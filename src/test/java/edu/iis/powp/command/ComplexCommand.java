f5b2d95b5f974fa689b2375e188ff5482f5f7a03
Piotr Markowski <piotrjakubmarkowski@gmail.com>
2017-04-26 22:17:09 +0200

    implementacja ComplexCommand

--- src/test/java/edu/iis/powp/command/ComplexCommand.java
+++ src/test/java/edu/iis/powp/command/ComplexCommand.java
@@ -11,12 +11,12 @@ public class ComplexCommand implements PlotterCommand{
 
     private List<PlotterCommand> commands;
 
+
     public ComplexCommand(List<PlotterCommand> commands) {
         this.commands = commands;
     }
 
-
     @Override public void execute(IPlotter iPlotter) {
-
+        commands.forEach(command -> command.execute(iPlotter));
     }
 }
